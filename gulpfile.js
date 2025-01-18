df59ddd6086ba33d8d405c98ebdd2c0f10a4dd83
Paul Heading <paulheading@me.com>
2025-01-18 14:59:50 +0000

    nicer readme workflow

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+import gulp from "gulp";
+import rename from "gulp-rename";
+
+let readme = {
+  dest: "src/markdown",
+  name: "README.md",
+  rename: "readme.md",
+  src: "README.md",
+  task: "copyREADME",
+};
+
+function copyfiles(done, file) {
+  gulp
+    .src(file.src)
+    .pipe(rename(file.rename ? file.rename : file.name))
+    .pipe(gulp.dest(file.dest));
+  done();
+}
+
+gulp.task(readme.task, function (done) {
+  copyfiles(done, readme);
+});
+
+gulp.task("watch", function (done) {
+  gulp.watch(readme.src, gulp.series([readme.task]));
+  done();
+});
