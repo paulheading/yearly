ae194f216fe19408efcf385626d78cdd4040b8f7
Paul Heading <paulheading@me.com>
2025-03-31 11:57:26 +0100

    improved param handling

---
+++ src/scripts/getters/getSearchParams.js
@@ -0,0 +1,42 @@
+import listenSearchParams from "~scripts/listeners/listenSearchParams";
+import getAccessToken from "~scripts/getters/getAccessToken";
+
+let state = { ...listenSearchParams(), loggedIn: getAccessToken() };
+
+function allScenarios(callback) {
+  if (this.fails) return;
+  callback(state);
+}
+
+function loggedIn(callback) {
+  if (this.fails || !state.loggedIn) return;
+  callback(state);
+}
+
+function loggedOut(callback) {
+  if (this.fails || state.loggedIn) return;
+  callback(state);
+}
+
+let validParams = {
+  fails: state.empty || !state.valid.length || state.invalid.length,
+  allScenarios,
+  loggedIn,
+  loggedOut,
+};
+
+let invalidParams = {
+  fails: state.empty || !state.invalid.length || state.valid.length,
+  allScenarios,
+  loggedIn,
+  loggedOut,
+};
+
+let mixedParams = {
+  fails: state.empty || !state.invalid.length || !state.valid.length,
+  allScenarios,
+  loggedIn,
+  loggedOut,
+};
+
+export default { validParams, invalidParams, mixedParams };
--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,37 +1,31 @@
 import $ from "~scripts/selectors";
-import getAccessToken from "~scripts/getters/getAccessToken";
 import displayBanner from "~scripts/display/displayBanner";
-import params from "~scripts/listeners/listenSearchParams";
 import createLogin from "~scripts/creators/createLogin";
 import printInvalidKeys from "~scripts/printers/printInvalidKeys";
 import printLoginPrompt from "~scripts/printers/printLoginPrompt";
 import usingLiveData from "~scripts/using/usingLiveData";
+import getSearchParams from "~scripts/getters/getSearchParams";
 
 if (!usingLiveData) displayBanner.innerHTML("<em>OFFLINE MODE</em>");
 
-// listen for params on load
+let { validParams, invalidParams, mixedParams } = getSearchParams;
 
-let { empty, valid, invalid } = params();
+validParams.loggedIn(() => window.location.assign("/build"));
 
-if (empty) {
-  console.log("no params");
-} else {
-  if (valid.length) {
-    if (getAccessToken()) {
-      // if valid AND logged in. go to build page
-      window.location.assign("/build");
-    } else {
-      // if valid AND logged out. show alert prompting log in
-      let content = printLoginPrompt();
-      displayBanner.append(content);
-    }
-  } else {
-    // if not valid. show confirm alert
-    if (invalid.length) {
-      let content = printInvalidKeys(invalid);
-      displayBanner.innerHTML(content);
-    }
-  }
-}
+validParams.loggedOut(function () {
+  let content = printLoginPrompt();
+  displayBanner.append(content);
+});
+
+invalidParams.allScenarios(function ({ invalid }) {
+  let content = printInvalidKeys(invalid);
+  displayBanner.innerHTML(content);
+});
+
+mixedParams.loggedOut(function () {
+  console.log("mixed");
+  let content = printLoginPrompt();
+  displayBanner.append(content);
+});
 
 $.button.login.addEventListener("click", createLogin);
